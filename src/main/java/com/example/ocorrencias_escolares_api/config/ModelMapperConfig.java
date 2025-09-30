85d3fc2626e58a15a7966afc6a41b9b36190f454
Pedro <pedrolucascabral4@gmail.com>
2025-09-30 16:05:11 -0300

    add Model Mapper bean configuration

--- src/main/java/com/example/ocorrencias_escolares_api/config/ModelMapperConfig.java
+++ src/main/java/com/example/ocorrencias_escolares_api/config/ModelMapperConfig.java
@@ -1,4 +1,14 @@
 package com.example.ocorrencias_escolares_api.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
