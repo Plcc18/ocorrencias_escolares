2c536054bc2751327e488a53ff66e28df9ed281c
Pedro <pedrolucascabral4@gmail.com>
2025-10-01 12:47:14 -0300

    add OpenAPI Swagger bean configuration update

--- src/main/java/com/example/ocorrencias_escolares_api/config/SwaggerConfig.java
+++ src/main/java/com/example/ocorrencias_escolares_api/config/SwaggerConfig.java
@@ -11,7 +11,7 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title('Ocorrências Escolares API')
+                        .title("Ocorrências Escolares API")
                         .version("1.0")
                         .description("API para gerenciamento de ocorrências escolares"));
     }
