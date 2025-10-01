40ee703681235378e104b99eb7ce40ac0fbe55e0
Pedro <pedrolucascabral4@gmail.com>
2025-10-01 12:46:01 -0300

    add OpenAPI Swagger bean configuration

--- src/main/java/com/example/ocorrencias_escolares_api/config/SwaggerConfig.java
+++ src/main/java/com/example/ocorrencias_escolares_api/config/SwaggerConfig.java
@@ -1,4 +1,18 @@
 package com.example.ocorrencias_escolares_api.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title('Ocorrências Escolares API')
+                        .version("1.0")
+                        .description("API para gerenciamento de ocorrências escolares"));
+    }
 }
