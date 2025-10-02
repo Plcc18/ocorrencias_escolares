06ea514486db21fd3b1eee62865e1036fcc837da
Pedro <pedrolucascabral4@gmail.com>
2025-10-01 21:37:36 -0300

    config entity Student

--- src/main/java/com/example/ocorrencias_escolares_api/entity/Student.java
+++ src/main/java/com/example/ocorrencias_escolares_api/entity/Student.java
@@ -1,4 +1,24 @@
 package com.example.ocorrencias_escolares_api.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Table(name = "students")
+@Data
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Size(max = 100)
+    private String name;
+
+    @NotBlank(message = "Grade is mandatory")
+    @Size(max = 20)
+    private String grade;
 }
