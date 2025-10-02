6e2025754faee3707c6ff2eddc87d1a06094ee33
Pedro <pedrolucascabral4@gmail.com>
2025-10-02 13:00:59 -0300

    feat StudentRepository

--- src/main/java/com/example/ocorrencias_escolares_api/repository/StudentRepository.java
+++ src/main/java/com/example/ocorrencias_escolares_api/repository/StudentRepository.java
@@ -1,4 +1,7 @@
 package com.example.ocorrencias_escolares_api.repository;
 
-public class StudentRepository {
+import com.example.ocorrencias_escolares_api.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
