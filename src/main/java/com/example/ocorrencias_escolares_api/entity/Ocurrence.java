eef65fbcd71759fdcbd04b0d0f4912aa6959427c
Pedro <pedrolucascabral4@gmail.com>
2025-10-02 09:04:09 -0300

    config entity Ocurrence

--- src/main/java/com/example/ocorrencias_escolares_api/entity/Ocurrence.java
+++ src/main/java/com/example/ocorrencias_escolares_api/entity/Ocurrence.java
@@ -1,4 +1,37 @@
 package com.example.ocorrencias_escolares_api.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "ocurrances")
+@Data
 public class Ocurrence {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Description is mandatory")
+    private String description;
+
+    @NotNull(message = "Date is mandatory")
+    private LocalDate ocuranceDate;
+
+    @NotNull(message = "Student is mandatory")
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+
+    @NotBlank(message = "Type is mandatory")
+    private String ocurrenceType;
 }
