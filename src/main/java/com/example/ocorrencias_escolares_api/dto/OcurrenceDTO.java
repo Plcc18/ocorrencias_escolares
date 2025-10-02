db1f905bde8c337a54386ae21171d8923e35961a
Pedro <pedrolucascabral4@gmail.com>
2025-10-02 12:35:20 -0300

    add DTO Ocurrence

--- src/main/java/com/example/ocorrencias_escolares_api/dto/OcurrenceDTO.java
+++ src/main/java/com/example/ocorrencias_escolares_api/dto/OcurrenceDTO.java
@@ -1,4 +1,28 @@
 package com.example.ocorrencias_escolares_api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class OcurrenceDTO {
+
+    private Long Id;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    private LocalDate ocurranceDate;
+
+    @NotNull
+    private Long studentId;
+
+    @NotNull
+    private Long teacherId;
+
+    @NotBlank
+    private String ocurrenceType;
 }
