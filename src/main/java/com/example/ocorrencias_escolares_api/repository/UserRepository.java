5e0021f5969a876d35f0ab1ca5958a8d395f8291
Pedro <pedrolucascabral4@gmail.com>
2025-10-02 12:44:34 -0300

    feat UserRepository

--- src/main/java/com/example/ocorrencias_escolares_api/repository/UserRepository.java
+++ src/main/java/com/example/ocorrencias_escolares_api/repository/UserRepository.java
@@ -1,4 +1,10 @@
-package com.example.ocorrencias_escolares_api.repository;
+    package com.example.ocorrencias_escolares_api.repository;
 
-public class UserRepository {
-}
+    import com.example.ocorrencias_escolares_api.entity.User;
+    import org.springframework.data.jpa.repository.JpaRepository;
+
+    import java.util.Optional;
+
+    public interface UserRepository extends JpaRepository<User, Long> {
+        Optional<User> findByUsername(String username);
+    }
