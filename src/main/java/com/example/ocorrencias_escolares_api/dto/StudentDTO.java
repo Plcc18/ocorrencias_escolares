2b7c6c6881562dfa9e8786aed8f6cf694de4364a
Pedro <pedrolucascabral4@gmail.com>
2025-10-02 09:15:18 -0300

    add DTO Student

--- src/main/java/com/example/ocorrencias_escolares_api/dto/StudentDTO.java
+++ src/main/java/com/example/ocorrencias_escolares_api/dto/StudentDTO.java
@@ -1,4 +1,19 @@
 package com.example.ocorrencias_escolares_api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class StudentDTO {
+
+    private Long id;
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+
+    @NotBlank
+    @Size(max = 20)
+    private String grade;
 }
