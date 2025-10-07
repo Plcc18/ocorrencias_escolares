7e4fcfacd7c22eeb568287d351fa7380950379fc
Pedro <pedrolucascabral4@gmail.com>
2025-10-07 11:16:13 -0300

    feat: implementing load user datails

--- src/main/java/com/example/ocorrencias_escolares_api/security/CustomUserDetailsService.java
+++ src/main/java/com/example/ocorrencias_escolares_api/security/CustomUserDetailsService.java
@@ -1,4 +1,23 @@
 package com.example.ocorrencias_escolares_api.security;
 
-public class CustomUserDetailsService {
+import com.example.ocorrencias_escolares_api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 }
