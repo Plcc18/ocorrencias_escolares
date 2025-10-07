6010c191b77e20b40c831d5dbf7b60c84efa117c
Pedro <pedrolucascabral4@gmail.com>
2025-10-06 23:31:16 -0300

    feat OccurrenceService

--- src/main/java/com/example/ocorrencias_escolares_api/service/OccurrenceService.java
+++ src/main/java/com/example/ocorrencias_escolares_api/service/OccurrenceService.java
@@ -1,4 +1,16 @@
 package com.example.ocorrencias_escolares_api.service;
 
-public class OccurrenceService {
+import com.example.ocorrencias_escolares_api.dto.OccurrenceDTO;
+import com.example.ocorrencias_escolares_api.dto.StudentDTO;
+import com.example.ocorrencias_escolares_api.entity.Occurrence;
+import com.example.ocorrencias_escolares_api.entity.Student;
+
+import java.util.List;
+
+public interface OccurrenceService {
+    Occurrence create(OccurrenceDTO dto);
+    Occurrence update(Long id, OccurrenceDTO dto);
+    Occurrence findById(Long id);
+    List<Occurrence> findAll();
+    void delete(Long id);
 }
