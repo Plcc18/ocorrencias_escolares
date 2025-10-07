8a64664039b49763e04d1899a780449fa8f51801
Pedro <pedrolucascabral4@gmail.com>
2025-10-07 20:10:26 -0300

    feat(auth): create JwtTokenProvider for token generation and validation

--- src/main/java/com/example/ocorrencias_escolares_api/security/JwtTokenProvider.java
+++ src/main/java/com/example/ocorrencias_escolares_api/security/JwtTokenProvider.java
@@ -1,4 +1,48 @@
 package com.example.ocorrencias_escolares_api.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.file.attribute.UserPrincipal;
+import java.util.Date;
+
+@Component
 public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    private final long jwtExpirationMs = 86400000; //24 hours
+
+    public String generateToken(Authentication authentication) {
+        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+        return Jwts.builder()
+                .subject(userPrincipal.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key)
+                .compact();
+    }
+
+    public String getUsernameFromJwt(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload().getSubject();
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(authToken);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
