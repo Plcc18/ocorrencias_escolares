c816f9321fa578463ad9b57a5bde78e8948acb22
Pedro <pedrolucascabral4@gmail.com>
2025-10-06 23:16:27 -0300

    feat TeacherService

--- src/main/java/com/example/ocorrencias_escolares_api/service/TeacherService.java
+++ src/main/java/com/example/ocorrencias_escolares_api/service/TeacherService.java
@@ -1,4 +1,15 @@
 package com.example.ocorrencias_escolares_api.service;
 
-public class TeacherService {
+import com.example.ocorrencias_escolares_api.dto.StudentDTO;
+import com.example.ocorrencias_escolares_api.entity.Student;
+import com.example.ocorrencias_escolares_api.entity.Teacher;
+
+import java.util.List;
+
+public interface TeacherService {
+    Teacher create(StudentDTO dto);
+    Teacher update(Long id, StudentDTO dto);
+    Teacher findById(Long id);
+    List<Teacher> findAll();
+    void delete(Long id);
 }
