6aa9c0ded65e5a23bde5bcea6219ff75405e954d
Pedro <pedrolucascabral4@gmail.com>
2025-10-15 01:22:47 -0300

    feat(exception): add ResourceNotFoundException for 404 errors

--- src/main/java/com/example/ocorrencias_escolares_api/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/ocorrencias_escolares_api/exception/ResourceNotFoundException.java
@@ -1,6 +1,11 @@
 package com.example.ocorrencias_escolares_api.exception;
 
-public class ResourceNotFoundException {
-    public ResourceNotFoundException(String studentNotFound) {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
 }
